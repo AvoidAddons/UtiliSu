5784e9c85ce64417b8dc73f68fc4e748f84fa9ee
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-12 11:17:02 +0200

    Initial commit

---
+++ src/main/java/pl/olafcio/utilisu/UtiliSu.java
@@ -0,0 +1,10 @@
+package pl.olafcio.utilisu;
+
+import pl.olafcio.avoid.mods.AvoidMod;
+
+public class UtiliSu extends AvoidMod {
+    @Override
+    public void onEnable() {
+        System.out.println("[UtiliSu] Mod enabled");
+    }
+}
