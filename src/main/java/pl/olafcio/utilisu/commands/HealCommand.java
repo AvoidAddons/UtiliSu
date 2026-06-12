f7d4c0d9125cc4798fa9ec45fde439ca6ebc466b
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-12 11:20:33 +0200

    /heal

---
+++ src/main/java/pl/olafcio/utilisu/commands/HealCommand.java
@@ -0,0 +1,30 @@
+package pl.olafcio.utilisu.commands;
+
+import pl.olafcio.avoid.mods.annotation_processor.AutoCommand;
+import pl.olafcio.avoid.net.chat.component.Colors;
+import pl.olafcio.avoid.net.chat.component.Components;
+import pl.olafcio.avoid.net.command.Command;
+import pl.olafcio.avoid.net.command.annotation.Syntax;
+import pl.olafcio.avoid.net.command.handling.Usage;
+import pl.olafcio.avoid.net.player.Player;
+
+@AutoCommand
+public class HealCommand extends Command {
+    @Syntax("/heal")
+    public void heal(Usage input) {
+        if (input.getExecutor() instanceof Player player) {
+            player.updateHealth(player.getMaxHealth());
+            player.updateFoodLevel(20);
+            player.updateFoodSaturation(5F);
+
+            if (player.isOnFire())
+                player.extinguishFire();
+
+            player.sendMessage(Components.literal("Healed.")
+                                         .color(Colors.GOLD));
+        } else {
+            input.getExecutor().sendMessage(Components.literal("Can only heal players")
+                                                      .color(Colors.RED));
+        }
+    }
+}
